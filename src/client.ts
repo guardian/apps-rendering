5d6cbe0a9ace2dbffcc45061d64f9b3c124720ff
Jamie Webb <jamie.webb@guardian.co.uk>
2019-10-24 16:49:05 +0100

    return promise to callee and resolve and reject at the right times

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,7 @@
-import { initNativeApi } from "native/nativeApi";
+import { initNativeApi, nativeCall } from "native/nativeApi";
 
 initNativeApi()
+
+setInterval(() => {
+    nativeCall('ads-ready').then(response => console.log(response))
+}, 4000);
--- src/native/nativeApi.ts
+++ src/native/nativeApi.ts
@@ -1,12 +1,14 @@
 declare global {
     interface Window {
         nativeCalls: Action[];
-        nativeCall: (command: string) => void;
+        nativeCall: (command: string) => Promise<string>;
         nativeHandler: (id: string, outcome: 'fulfilled' | 'rejected', response: string) => void;
         AndroidWebViewMessage: (command: string) => {};
         webkit: {
             messageHandlers: {
-                iOSWebViewMessage: (command: string) => {};
+                iOSWebViewMessage: {
+                    postMessage: ({ command, id }: { command: string, id: string }) => {};
+                };
             };
         };
     }
@@ -16,39 +18,46 @@ interface Action {
     id: string;
     timestamp: number;
     promise: Promise<string>;
+    resolve: any;
+    reject: any;
 }
 
 const ACTION_TIMEOUT_MS = 300000;
 
-function nativeCall(command: string): void {
+function nativeCall(command: string): Promise<string> {
+    let resolve, reject;
     const timestamp = Date.now();
     const id = Math.random().toString().split('.')[1];
-    const promise = new Promise<string>(function(_resolve, reject): void {
+    const promise = new Promise<string>(function(res, rej): void {
+        resolve = res;
+        reject = rej;
         setTimeout(function() {
-            reject('timeout');
+            rej('timeout');
         }, ACTION_TIMEOUT_MS);
     })
 
-    window.nativeCalls.push({ id, promise, timestamp });
+    window.nativeCalls.push({ id, promise, timestamp, resolve, reject });
 
     if (window.AndroidWebViewMessage) {
         window.AndroidWebViewMessage(command)
     } else if (window.webkit
         && window.webkit.messageHandlers
         && window.webkit.messageHandlers.iOSWebViewMessage) {
-            window.webkit.messageHandlers.iOSWebViewMessage(command)
+            window.webkit.messageHandlers.iOSWebViewMessage.postMessage({ command, id })
     } else {
         console.warn('No native APIs available');
     }
+
+    return promise;
 } 
 
 function nativeHandler(id: string, outcome: 'fulfilled' | 'rejected', response: string): void {
     const action = window.nativeCalls.find((action: Action) => action.id === id);
     if (!action) return;
     if (outcome === 'fulfilled') {
-        Promise.resolve(response);
+        action.resolve(response)
     } else {
-        Promise.reject(response);
+        action.reject()
     }
     window.nativeCalls = window.nativeCalls.filter((action: Action) => action.id !== id);
 }
@@ -65,4 +74,4 @@ function initNativeApi(): void {
     }, ACTION_TIMEOUT_MS);
 }
 
-export { initNativeApi };
+export { initNativeApi, nativeCall };
