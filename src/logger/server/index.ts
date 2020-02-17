ff0d7003ccd1f8031590a87424be45c75148e8c1
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-02-17 16:10:51 +0000

    Add a new server side logger

--- src/logger/server/index.ts
+++ src/logger/server/index.ts
@@ -1,20 +1,69 @@
 import {ILogger} from "../ILogger";
+import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+import {App, Stack, Stage} from "../../server/appIdentity";
 
 class ServerLogger implements ILogger {
+
+    underlyingLogger: winston.Logger;
+
+    constructor() {
+        let fileTransport;
+        if (process.env.NODE_ENV === 'production') {
+            fileTransport = new DailyRotateFile({
+                filename: `${App}.log`,
+                dirname: `/var/log/${App}`,
+                zippedArchive: true,
+                maxSize: '50m',
+                maxFiles: '7d',
+                utc: true
+            });
+        } else {
+            fileTransport = new winston.transports.Console({
+                format: winston.format.simple()
+            });
+        }
+
+        this.underlyingLogger = winston.createLogger({
+            level: 'info',
+            format: winston.format.json(),
+            defaultMeta: {
+                app: App,
+                stack: Stack,
+                stage: Stage
+            },
+            transports: [fileTransport]
+        });
+    }
+
     debug(message: string): void {
-        console.debug(message);
+        this.underlyingLogger.log({
+            level: 'debug',
+            message
+        })
     }
 
     info(message: string): void {
-        console.info(message);
+        this.underlyingLogger.log({
+            level: 'info',
+            message
+        })
     }
 
     warn(message: string, error?: Error): void {
-        console.warn(message, error);
+        this.underlyingLogger.log({
+            level: 'warn',
+            message,
+            stack_trace: error?.stack
+        })
     }
 
     error(message: string, error?: Error): void {
-        console.error(message, error);
+        this.underlyingLogger.log({
+            level: 'error',
+            message,
+            stack_trace: error?.stack
+        })
     }
 }
 
