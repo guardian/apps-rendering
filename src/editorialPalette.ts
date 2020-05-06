c00f955a7a5da1a058f68fb1ef06529305068e4b
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-05-06 11:57:13 +0100

    Added editorialPalette module; used for headline
    - Created editorialPalette module
    - Functions to get headlineFont, headlineBackground and border colours
    - Functions to apply color and background declarations
    - Applied new palette functions to headline component

---
+++ src/editorialPalette.ts
@@ -0,0 +1,111 @@
+// ----- Imports ----- //
+
+import { SerializedStyles, css } from '@emotion/core';
+import {
+    text,
+    background,
+    neutral,
+    news,
+    opinion,
+    sport,
+    culture,
+    lifestyle,
+} from '@guardian/src-foundations/palette';
+import { Format, Design, Display, Pillar } from '@guardian/types/Format';
+
+import { compose } from 'lib';
+
+
+// ----- Types ----- //
+
+interface Colour {
+    light: string;
+    dark: string;
+}
+
+
+// ----- Functions ----- //
+
+const headlineFont = (format: Format): Colour => {
+    const light = text.primary;
+    const dark = neutral[86];
+
+    if (format.display === Display.Immersive || format.design === Design.Media) {
+        return ({ light: neutral[100], dark });
+    }
+
+    if (format.design === Design.Feature) {
+        switch (format.pillar) {
+            case Pillar.Opinion:
+                return ({ light: opinion[300], dark });
+            case Pillar.Sport:
+                return ({ light: sport[300], dark });
+            case Pillar.Culture:
+                return ({ light: culture[300], dark });
+            case Pillar.Lifestyle:
+                return ({ light: lifestyle[300], dark });
+            case Pillar.News:
+            default:
+                return ({ light: news[300], dark });
+        }
+    }
+
+    return { light, dark };
+}
+
+const headlineBackground = (format: Format): Colour => {
+    const light = background.primary;
+    const dark = background.inverse;
+
+    if (format.display === Display.Immersive) {
+        return ({ light: neutral[7], dark });
+    }
+
+    return { light, dark };
+}
+
+const border = (format: Format): Colour => {
+    switch (format.pillar) {
+        case Pillar.Opinion:
+            return ({ light: opinion[400], dark: opinion[400] });
+        case Pillar.Sport:
+            return ({ light: sport[400], dark: sport[400] });
+        case Pillar.Culture:
+            return ({ light: culture[400], dark: culture[400] });
+        case Pillar.Lifestyle:
+            return ({ light: lifestyle[400], dark: lifestyle[400] });
+        case Pillar.News:
+        default:
+            return ({ light: news[400], dark: news[400] });
+    }
+}
+
+const fontColour = (colour: Colour): SerializedStyles =>
+    css`
+        color: ${colour.light};
+
+        @media (prefers-color-scheme: dark) {
+            color: ${colour.dark};
+        }
+    `;
+
+const backgroundColour = (colour: Colour): SerializedStyles =>
+    css`
+        background-color: ${colour.light};
+
+        @media (prefers-color-scheme: dark) {
+            background-color: ${colour.dark};
+        }
+    `;
+
+const headlineFontColour = compose(fontColour, headlineFont);
+const headlineBackgroundColour = compose(backgroundColour, headlineBackground);
+
+
+// ----- Exports ----- //
+
+export {
+    headlineFontColour,
+    headlineBackgroundColour,
+    border,
+};
