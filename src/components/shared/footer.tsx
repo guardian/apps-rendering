b73ca569d2bdd4d4dc3ae5ae26e6751b3ff62aa2
Demetrios Skamiotis <dskamiotis@gmail.com>
2020-11-17 14:28:05 +0000

    added policy link

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -50,7 +50,17 @@ const renderContent = (ccpaStatus: boolean): JSX.Element | null => {
 			</>
 		);
 	} else {
-		return null;
+		return (
+			<>
+				<a
+					css={anchor}
+					href="https://www.theguardian.com/help/privacy-policy"
+				>
+					Privacy Settings
+				</a>
+				&nbsp;&#183;&nbsp;
+			</>
+		);
 	}
 };
 
