eb49110e1a6517e4855460c171194f8ff7a199f0
Joe Cowton <joe.cowton@guardian.co.uk>
2021-01-15 12:20:17 +0000

    feat(editions-article): add border to header components

--- src/components/editions/article.tsx
+++ src/components/editions/article.tsx
@@ -3,13 +3,13 @@
 import { css } from '@emotion/core';
 import { remSpace } from '@guardian/src-foundations';
 import { from } from '@guardian/src-foundations/mq';
-import { border } from '@guardian/src-foundations/palette';
+import { background, border } from '@guardian/src-foundations/palette';
 import { Design, partition } from '@guardian/types';
 import type { Item } from 'item';
 import type { FC } from 'react';
 import { renderEditionsAll } from 'renderer';
-import { wideContentWidth } from 'styles';
 import Header from './header';
+import { editionsArticleWidth } from './styles';
 
 // ----- Component ----- //
 
@@ -17,9 +17,38 @@ interface Props {
 	item: Item;
 }
 
+const articleStyles = css`
+	padding-left: ${remSpace[2]};
+	padding-right: ${remSpace[2]};
+
+	${from.phablet} {
+		padding-left: 0;
+		padding-right: 0;
+	}
+`;
+
 const bodyStyles = css`
 	border-top: 1px solid ${border.secondary};
 	padding-top: ${remSpace[4]};
+
+	figcaption {
+		background: ${background.primary};
+		padding-bottom: ${remSpace[2]};
+	}
+
+	p {
+		padding-right: ${remSpace[1]};
+	}
+
+	${from.phablet} {
+		border-right: 1px solid ${border.secondary};
+
+		p {
+			margin: 0;
+			padding-top: ${remSpace[2]};
+			padding-bottom: ${remSpace[2]};
+		}
+	}
 
 	${from.wide} {
 		margin: 0 auto;
@@ -27,7 +56,7 @@ const bodyStyles = css`
 
 	${from.phablet} {
 		margin-left: ${remSpace[24]};
-		width: ${wideContentWidth}px;
+		width: ${editionsArticleWidth}rem;
 	}
 `;
 
@@ -38,7 +67,7 @@ const Article: FC<Props> = ({ item }) => {
 
 	return (
 		<main>
-			<article>
+			<article css={articleStyles}>
 				<Header item={item} />
 				<section css={[bodyStyles]}>
 					{renderEditionsAll(item, partition(item.body).oks)}
