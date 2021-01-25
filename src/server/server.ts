338900cabab8125140ec0bc3bf596c2e990eb252
Anna Beddow <anna.beddow@gmail.com>
2021-01-25 14:16:38 +0000

    use Editions param

--- src/server/server.ts
+++ src/server/server.ts
@@ -170,7 +170,7 @@ async function serveArticlePost(
 ): Promise<void> {
 	try {
 		const renderingRequest = await mapiDecoder(req.body);
-		const isEditions = req.query.testingParam === '';
+		const isEditions = req.query.editions === '';
 		const richLinkDetails = req.query.richlink === '';
 
 		if (richLinkDetails) {
