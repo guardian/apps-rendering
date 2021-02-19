0f5bff65e2cc397607ef2ff45d3e259a2ed4b8e1
Demetrios Skamiotis <dskamiotis@gmail.com>
2021-02-19 15:57:37 +0000

    amended theme to labs

--- src/components/paragraph.tsx
+++ src/components/paragraph.tsx
@@ -4,8 +4,7 @@ import type { SerializedStyles } from '@emotion/core';
 import { css } from '@emotion/core';
 import { remSpace } from '@guardian/src-foundations';
 import { body, textSans } from '@guardian/src-foundations/typography';
-import type { Format } from '@guardian/types';
-import { Design } from '@guardian/types';
+import { Format, Special, Theme } from '@guardian/types';
 import type { FC, ReactNode } from 'react';
 
 // ----- Component ----- //
@@ -15,21 +14,20 @@ interface Props {
 	format: Format;
 }
 
-const styles = (design: Design): SerializedStyles => {
-	const advertisementFeature =
-		design === Design.AdvertisementFeature ? textSans.medium() : null;
+const styles = (theme: Theme): SerializedStyles => {
+	const labs = theme === Special.Labs ? textSans.medium() : null;
 
 	return css`
 		${body.medium()}
 		overflow-wrap: break-word;
 		margin: 0 0 ${remSpace[3]};
 
-		${advertisementFeature}
+		${labs}
 	`;
 };
 
 const Paragraph: FC<Props> = ({ children, format }: Props) => (
-	<p css={styles(format.design)}>{children}</p>
+	<p css={styles(format.theme)}>{children}</p>
 );
 
 // ----- Exports ----- //
