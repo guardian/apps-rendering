a425d41b3ee7cd34f782b5a65499632d8bc98951
Marjan K <marjank@30728.gnm.int>
2021-04-28 13:08:43 +0100

    removed unwanted files

--- src/components/notFound.tsx
+++ src/components/notFound.tsx
@@ -1,5 +1,9 @@
 import { css } from '@emotion/react';
+import { brand } from '@guardian/src-foundations/palette';
 import { FC } from 'react';
+import type { SerializedStyles } from '@emotion/react';
+import { remSpace } from '@guardian/src-foundations';
+import { from } from '@guardian/src-foundations/mq';
 
 const tabletSquare1 = css`
 	position: absolute;
@@ -21,17 +25,17 @@ const tabletSquare2 = css`
 	box-sizing: border-box;
 `;
 
-const headline = css`
-	font-style: normal;
-	font-weight: 500;
-	font-size: 24px;
-	line-height: 130%;
-	position: absolute;
-	width: 284px;
-	height: 84px;
-	left: 12px;
-	top: 105px;
-`;
+const headline = css`{
+	color: Palette.neutral[7],
+	fontSize: '28px',
+	fontWeight: 200,
+	lineHeight: 1.15,
+	marginBottom: '12px',
+	maxWidth: '540px',	
+	${from.tablet} {
+		fontSize: '42px',
+	},
+}`;
 
 const subHeadline = css`
 	font-style: normal;
@@ -45,12 +49,14 @@ const subHeadline = css`
 	left: 12px;
 	top: 210px;
 `;
+
 const reportLink = css`
 	color: #ffffff;
 	text-decoration: none;
 	border-bottom: 1px solid #ffffff;
 	padding-bottom: 3px; ;
 `;
+
 const arrowPosition = css`
 	position: absolute;
 	width: 270px;
@@ -58,6 +64,7 @@ const arrowPosition = css`
 	left: 12px;
 	top: 319px;
 `;
+
 const arrow = css`
 	background-color: #ffffff;
 	border-radius: 9999px;
@@ -74,6 +81,7 @@ const arrow = css`
 	position: relative;
 	text-decoration: none;
 `;
+
 const ovalContainer = css`
 	height: 44px;
 	position: absolute;
@@ -88,50 +96,117 @@ const ovalContainer = css`
 	width: 40px;
 `;
 
-const body = css`
-	background-color: #052962;
+const gsContainer = css`
+	position: relative;
+	margin: 0 auto;
+	z-index: 5000;
+	padding: 0 10px;
 `;
 
-const gsContainer = css``;
+const Styles = css`
+	background: ${brand[400]};
+`;
+
+export const sidePadding = css`
+	padding-left: ${remSpace[2]};
+	padding-right: ${remSpace[2]};
+
+	${from.wide} {
+		padding-left: 0;
+		padding-right: 0;
+	}
+`;
+
+export const wideContentWidth = 620;
+
+export const articleWidthStyles: SerializedStyles = css`
+	${sidePadding}
+	${from.wide} {
+		margin: 0 auto;
+	}
+
+	${from.phablet} {
+		width: ${wideContentWidth}px;
+	}
+`;
 
 const NotFound: FC = () => (
-	<div css={body}>
-		{/* <div css={gsContainer}>
-			<div css={tabletSquare1}></div>
-			<div css={tabletSquare2}></div>
-			<div css={headline}>
-				Sorry – we haven’t been able to serve the page you asked for.
-			</div>
-			<div css={subHeadline}>
-				You may have followed an outdated link, or have mistyped a URL.
-				If you believe this to be an error please{' '}
-				<a
-					css={reportLink}
-					href="https://www.theguardian.com/info/tech-feedback"
-				>
-					report it
-				</a>
-				.
-			</div>
-			<div css={arrowPosition}>
-				<a href="https://www.theguardian.com/" css={arrow}>
-					The Guardian homepage
-					<svg
-						width="30"
-						height="30"
-						viewBox="0 0 30 30"
-						css={ovalContainer}
+	<main css={[Styles]}>
+		<div css={articleWidthStyles}>
+			<div css={gsContainer}>
+				<div css={tabletSquare1}></div>
+				<div css={tabletSquare2}></div>
+				<div css={headline}>
+					Sorry – we haven’t been able to serve the page you asked
+					for.
+				</div>
+				<div css={subHeadline}>
+					You may have followed an outdated link, or have mistyped a
+					URL. If you believe this to be an error please{' '}
+					<a
+						css={reportLink}
+						href="https://www.theguardian.com/info/tech-feedback"
 					>
-						<path
-							d="M22.8 14.6L15.2 7l-.7.7 5.5 6.6H6v1.5h14l-5.5 6.6.7.7 7.6-7.6v-.9"
-							fill="#052962"
-						></path>
-					</svg>
-				</a>
+						report it
+					</a>
+					.
+				</div>
+				<div css={arrowPosition}>
+					<a href="https://www.theguardian.com/" css={arrow}>
+						The Guardian homepage
+						<svg
+							width="30"
+							height="30"
+							viewBox="0 0 30 30"
+							css={ovalContainer}
+						>
+							<path
+								d="M22.8 14.6L15.2 7l-.7.7 5.5 6.6H6v1.5h14l-5.5 6.6.7.7 7.6-7.6v-.9"
+								fill="#052962"
+							></path>
+						</svg>
+					</a>
+				</div>
 			</div>
+			{/* {
+				<div css={gsContainer}>
+					<div css={tabletSquare1}></div>
+					<div css={tabletSquare2}></div>
+					<div css={headline}>
+						Sorry – we haven’t been able to serve the page you asked
+						for.
+					</div>
+					<div css={subHeadline}>
+						You may have followed an outdated link, or have mistyped
+						a URL. If you believe this to be an error please{' '}
+						<a
+							css={reportLink}
+							href="https://www.theguardian.com/info/tech-feedback"
+						>
+							report it
+						</a>
+						.
+					</div>
+					<div css={arrowPosition}>
+						<a href="https://www.theguardian.com/" css={arrow}>
+							The Guardian homepage
+							<svg
+								width="30"
+								height="30"
+								viewBox="0 0 30 30"
+								css={ovalContainer}
+							>
+								<path
+									d="M22.8 14.6L15.2 7l-.7.7 5.5 6.6H6v1.5h14l-5.5 6.6.7.7 7.6-7.6v-.9"
+									fill="#052962"
+								></path>
+							</svg>
+						</a>
+					</div>
+				</div>
+			} */}
 		</div>
-	 */}
-	</div>
+	</main>
 );
 
 export default NotFound;
