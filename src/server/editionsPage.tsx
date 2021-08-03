15811a13e0077e0addb408d95847b43ff11266d3
Mohammad Haque <mohammad.haque@guardian.co.uk>
2021-08-03 13:33:20 +0100

    fixed lint

--- src/server/editionsPage.tsx
+++ src/server/editionsPage.tsx
@@ -159,12 +159,12 @@ function render(
 	const item = fromCapi({ docParser, salt: imageSalt })(request);
 
 	// remove these once debuggin is complete
-	logger.info('IS PREVIEW:' + isPreview);
-	logger.info('NODE ENV:' + process.env.NODE_ENV);
-	logger.info('PATH:' + path);
-	logger.info('stage:' + Stage);
-	logger.info('ENV:' + environment);
-	logger.info('ITEM ID:' + item.internalShortId);
+	logger.info(`IS PREVIEW: ${isPreview}`);
+	logger.info(`NODE ENV: ${process.env.NODE_ENV}`);
+	logger.info(`PATH: ${path}`);
+	logger.info(`stage: ${Stage}` );
+	logger.info(`ENV: ${environment}`);
+	logger.info(`ITEM ID: ${item.internalShortId}`);
 
 	const newItem = {
 		...item,
