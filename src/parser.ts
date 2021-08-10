585787f50d837f2cb4ae46ecb01eb893afd92b2f
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-08-10 14:57:32 +0100

    Lint fix

--- src/parser.ts
+++ src/parser.ts
@@ -699,7 +699,7 @@ const andThen = <A, B>(f: (a: A) => Parser<B>) => (pa: Parser<A>): Parser<B> =>
  * another until a) one of them succeeds, or b) the end of the list is reached
  * with none successful. If none are successful then all the error messages
  * will be concatenated into a final error.
- * 
+ *
  * @param parsers A list of parsers to try one after another
  * @returns {Parser<A>} A new parser for type `A`
  * @example
@@ -708,20 +708,22 @@ const andThen = <A, B>(f: (a: A) => Parser<B>) => (pa: Parser<A>): Parser<B> =>
  *   map(parseFloat),
  *   andThen(numberParser),
  * );
- * 
+ *
  * const parser = oneOf([numberParser, stringAsNumberParser]); // Parser<number>
- * 
+ *
  * const jsonA: unknown = JSON.parse('42');
  * const resultA = parse(parser)(jsonA); // Ok<number>, 42
- * 
+ *
  * const jsonB: unknown = JSON.parse('"42"');
  * const resultB = parse(parser)(jsonB); // Ok<number>, 42
- * 
+ *
  * const jsonC: unknown = JSON.parse('null');
- * const resultC = parse(parser)(jsonC); // Err<string>, "null is not a valid number. null is not a valid string"
- * 
+ * // Err<string>, "null is not a valid number. null is not a valid string"
+ * const resultC = parse(parser)(jsonC);
+ *
  * const jsonD: unknown = JSON.parse('"foo"');
- * const resultD = parse(parser)(jsonD); // Err<string>, "'foo' is not a valid number. NaN is not a valid number"
+ * // Err<string>, "'foo' is not a valid number. NaN is not a valid number"
+ * const resultD = parse(parser)(jsonD); 
  */
 const oneOf = <A>(parsers: Array<Parser<A>>): Parser<A> =>
 	parser((a) => {
